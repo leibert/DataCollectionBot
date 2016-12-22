28af7054e83e02c698050e374882147687abd4f5
William Cummings <will@wpc.io>
2016-12-22 10:26:37 -0500

    Add setup.py to satiate pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='datacollcetionbot',
+      version='0.1',
+      url='http://github.com/leibert/datacollectionbot',
+      author='leibert',
+      packages=['datacollectionbot'],
+      zip_safe=False)
